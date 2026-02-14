dbf3626943cd0259d33872e398b35a659412aed6
misavoltage <misa@misavoltage.dev>
2026-02-14 17:50:47 -0300

    feat(server): uses `logger.lua`

---
+++ server.lua
@@ -0,0 +1,8 @@
+local logger = require("logger").setup({
+	name = "server.lua",
+})
+
+logger.info("Hello, World!")
+logger.debug("Hello, World!")
+logger.warn("Hello, World!")
+logger.error("Hello, World!")
